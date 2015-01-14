5c05716c83a491acd5c8e2183f06b14a2c223c79
Alex <alex.king@surgemedia.com.au>
2015-01-15 09:56:25 +1000

    Imported Assets
    Unzipped and Imported all relevant  scripts

---
+++ assets/js/plugins/home.js
@@ -0,0 +1,177 @@
+$(document).ready(function() {
+
+	var isMobile = false,
+		mobileBreakpoint = 768;
+
+	if (window.innerWidth <= mobileBreakpoint) {
+		isMobile = true;
+	}
+
+	var homeEl = document.querySelector('.home'),
+		pageScroll,
+		scrollLocked = false,
+		sectionList,
+		sectionNext,
+		sectionIdx,
+		prevDeltaY = 0;
+
+	if (homeEl) {
+		init();
+
+		//To do: tidy up resize
+		var timeout = null;
+
+		window.onresize = function() {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+			timeout = setTimeout(function() {
+				if ($(window).width() <= mobileBreakpoint) {
+					isMobile = true;
+				} else {
+					isMobile = false;
+				}
+
+				init();
+			}, 250);
+		};
+	}
+
+	function initVideo() {
+		// To do: tidy up
+		$(".video-modal").click(function(e) {
+			e.preventDefault();
+
+			var vidSrc = this.getAttribute('data-src');
+			var $frame = $("#videoModal").find('.embed-responsive > iframe');
+
+			$frame.attr('src', '');
+			$frame.attr('src', vidSrc);
+			$($(this).attr('data-modal')).modal('show');
+
+		});
+
+		$("#videoModal").on('hidden.bs.modal', function(e) {
+			var $frame = $(this).find('.embed-responsive > iframe');
+
+			// saves the current iframe source
+			//var vidsrc = $frame.attr('src');
+			// sets the source to nothing, stopping the video
+			$frame.attr('src', '');
+
+			// sets it back to the correct link so that it reloads immediately on the next window open
+			//$frame.attr('src', vidsrc);
+		});
+	}
+
+	function init() {
+		initVideo();
+
+		var mainEl = document.getElementById('content-container'),
+			scrollerWrapperEl = document.querySelector('.scroller-wrapper'),
+			headerEl = document.getElementById('header'),
+			scrollerEl = document.querySelector('.scroller');
+		console.log(mainEl+scrollerWrapperEl+headerEl+scrollerEl)
+		sectionList = document.querySelectorAll('div.section');
+		sectionNext = document.querySelectorAll('div.section-footer');
+
+		sectionIdx = 0;
+		height = mainEl.offsetHeight;
+
+		// Calculate and set section dimensions
+		for (var i = 0; i < sectionList.length; i++) {
+			var section = sectionList[i];
+
+			section.style.width = mainEl.offsetWidth + 'px';
+
+			if (!isMobile) {
+				section.style.height = (window.innerHeight - headerEl.offsetHeight) + 'px';
+			} else {
+				section.style.height = 'auto';
+				height += section.offsetHeight;
+				console.log(height);
+			}
+		}
+
+		scrollerWrapperEl.style.height = window.innerHeight + 'px';
+
+		scrollerEl.style.height = !isMobile ? (sectionList.length * window.innerHeight) + 'px' : height + 'px';
+
+
+
+		if (pageScroll) {
+			pageScroll.destroy();
+		}
+
+		pageScroll = new IScroll('.scroller-wrapper', {
+			mouseWheel: !isMobile ? false : true,
+			deceleration: 0.01,
+			scrollY: true,
+			scrollX: false,
+			snap: !isMobile ? true : false
+		});
+
+		pageScroll.on('scrollStart', function() {
+			scrollLocked = true;
+		});
+
+		pageScroll.on('scrollEnd', function() {
+			if (scrollLocked) {
+				scrollLocked = false;
+			}
+		});
+
+		for (var i = 0; i < sectionNext.length; i++) {
+			var next = sectionNext[i];
+
+			next.onclick = function() {
+				pageScroll.scrollBy(0, -(window.innerHeight - headerEl.offsetHeight), 500);
+			}
+		}
+
+		//debounce timeout prevent event from firing consecutively
+		var scrollTimeout;
+
+		if (!isMobile) {
+			document.body.onmousewheel = function(e) {
+				e.preventDefault();
+
+				if (scrollTimeout) {
+					clearTimeout(scrollTimeout);
+				}
+
+				scrollTimeout = setTimeout(function() {
+					// check if scrolling direction is different
+					if ((e.deltaY <= 0 && prevDeltaY > 0) || (e.deltaY >= 0 && prevDeltaY <= 0)) {
+						scrollLocked = false;
+					}
+
+					if (!scrollLocked) {
+						//lock scroll right before animation, 'scrollStart' event cannot be used because of prevent default
+						scrollLocked = true;
+
+						if (e.deltaY > 0) {
+							sectionIdx++;
+
+							if (sectionIdx >= sectionList.length) {
+								sectionIdx = (sectionList.length - 1);
+							}
+
+							pageScroll.scrollToElement(sectionList[sectionIdx], 750);
+						} else {
+							sectionIdx--;
+
+							if (sectionIdx < 0) {
+								sectionIdx = 0;
+							}
+
+							pageScroll.scrollToElement(sectionList[sectionIdx], 750);
+						}
+
+						prevDeltaY = e.deltaY;
+					}
+				}, 50);
+			}
+		}
+	}
+});
--- templates/template-block-slider.php
+++ templates/template-block-slider.php
@@ -1,4 +1,4 @@
-        <div class="section hero pink" style="width: 1680px; height: 806px;">
+        <div class="section hero pink" >
           <img data-src="holder.js/1440x475/auto/gray" alt="1440x475" class="img-responsive" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQ0MCIgaGVpZ2h0PSI0NzUiIHZpZXdCb3g9IjAgMCAxNDQwIDQ3NSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSIxNDQwIiBoZWlnaHQ9IjQ3NSIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjUxOC4zMTI1IiB5PSIyMzcuNSIgc3R5bGU9ImZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZTo2OHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0NDB4NDc1PC90ZXh0PjwvZz48L3N2Zz4=" data-holder-rendered="true">
           <div class="hero-overlay"></div>
         
