f888e193de7187569d41eb5715e90ceb5f22dc21
Alex <alex.king@surgemedia.com.au>
2015-01-15 11:59:42 +1000

    Post Type - Videos
    Declares custom post type videos

---
+++ lib/action-post-type-videos.php
@@ -0,0 +1,46 @@
+<?php 
+// Register Custom Post Type
+function videos_post() {
+
+	$labels = array(
+		'name'                => _x( 'Videos', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Video', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Videos', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All video', 'text_domain' ),
+		'view_item'           => __( 'View Item', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Video', 'text_domain' ),
+		'add_new'             => __( 'Add New Video', 'text_domain' ),
+		'edit_item'           => __( 'Edit Item', 'text_domain' ),
+		'update_item'         => __( 'Update Item', 'text_domain' ),
+		'search_items'        => __( 'Search Item', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'videoes', 'text_domain' ),
+		'description'         => __( 'Video Posts', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'thumbnail', ),
+		'taxonomies'          => array( 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'slug'                => 'videos',
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 20,
+		'menu_icon'           => 'dashicons-format-video',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'videoes', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'videos_post', 0 );
