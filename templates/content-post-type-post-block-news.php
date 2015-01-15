4cedc28e69d6fa06e1df3ff4f61bd1cf28a733c5
Alex <alex.king@surgemedia.com.au>
2015-01-16 09:27:40 +1000

    Post Type - News - Changed Date System

--- templates/content-post-type-post-block-news.php
+++ templates/content-post-type-post-block-news.php
@@ -1,3 +1,8 @@
 <h3><a href="#"><span class="graphic arrow-link"></span><?php the_title(); ?></a></h3>
 <p><?php the_excerpt(); ?></p>
-<p class="date">[<?php echo get_the_date('F j'); ?>]</p>
+<p class="date">
+	[<?php
+		//Used post date but also have control for date published.
+	 	echo get_the_date('F j'); 
+	 ?>]
+</p>
