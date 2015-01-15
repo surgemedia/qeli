5984ab728b9bc3aebdb7efb98b190ea586f1a72c
Alex <alex.king@surgemedia.com.au>
2015-01-15 11:57:11 +1000

    Added Demo Site HTML Structure - WIP

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,78 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="" role="contentinfo">
+     <div class="container">
+        <div class="row">
+            <div class="col-xs-4">
+                <a id="back-to-top" href="#" class="back-to-top"><span class="graphic nav-back-to-top"></span> Back to top</a>
+            </div>
+            <div class="col-xs-4">
+                <div class="graphic footer-logo"></div>
+            </div>
+
+             <div class="col-xs-4">
+                <a href="#" class="graphic icon-share"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-qeli"></div>
+                <h2>QELI</h2>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-about"></div>
+                <h2>About us</h2>
+                <ul>
+                    <li><a href="#">CEO and Board</a></li>
+                    <li><a href="#">Ambassadors</a></li>
+                    <li><a href="#">Results and Outcomes</a></li>
+                    <li><a href="#">Publications</a></li>
+                    <li><a href="#">Contact QELi</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-courses"></div>
+                <h2>Our programs</h2>
+                <ul>
+                    <li><a href="#">Programs overview</a></li>
+                    <li><a href="#">Course example</a></li>
+                    <li><a href="#">Course cart</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-news"></div>
+                <h2>News/Resources</h2>
+                <ul>
+                    <li><a href="#">Newsletters</a></li>
+                    <li><a href="#">Media Releases</a></li>
+                    <li><a href="#">QELi Talks</a></li>
+                    <li><a href="#">Sector News</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-legal"></div>
+                <h2>Legal</h2>
+                <ul>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Terms of use</a></li>
+                    <li><a href="#">Disclaimer</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="graphic footer-sitemap"></div>
+                <h2><a href="#">Sitemap</a></h2>
+            </div>
+        </div>       
+    </div>
+    <div class="copyright">
+        <div class="container"> 
+            <div class="row">
+            ©2014 QELi. All rights reserved  |  credits / breadcrumb digital (build) /  volt (design)
+            </div>
+        </div>
+    </div>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,5 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<?php get_template_part('templates/part', 'trimming-stripe-rainbow'); ?>
+<header id="header" class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -13,7 +14,13 @@
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
+          wp_nav_menu(
+            array('theme_location' => 'primary_navigation',
+           'walker' => new Roots_Nav_Walker(),
+           'menu_class' => 'nav navbar-nav',
+           'link_before'     => '<span class="graphics"></span><span class="nav-label">',
+            'link_after'      => '</span>',
+           ));
         endif;
       ?>
     </nav>
