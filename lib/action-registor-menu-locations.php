dfe4e5d05b5ada3ba4e64f6371f32090e66030a1
Alex <alex.king@surgemedia.com.au>
2015-01-23 11:35:11 +1000

    Nav menus - Renamed lables in backend

--- lib/action-registor-menu-locations.php
+++ lib/action-registor-menu-locations.php
@@ -1,5 +1,5 @@
 <?php 
 register_nav_menus( array(
-	'search' => 'Search section on the right',
-	'footer' => 'The menu in the footer',
+	'search' => 'Search & Newsletter',
+	'footer' => 'Footer Navigation',
 ) );
