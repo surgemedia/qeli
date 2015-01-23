27b3c6b210393018fff163a7669a7fcccbb0eaa6
Alex <alex.king@surgemedia.com.au>
2015-01-23 11:34:15 +1000

    Review - Make Master Post on Course - Function
    @jwaterer

--- lib/function-only-this-post.php
+++ lib/function-only-this-post.php
@@ -7,31 +7,29 @@ function my_admin_notice() {
 /*=============================================
 	=    	Make Master Post on course        =
 =============================================*/
-//$post_ids,$field_name,$sub_field,$post_type
 function removeOtherPosts($post_type){
 	if(is_admin() && isset($_GET['post'])){
-	
 	if(true == isset($_GET['post'])){ $post_id = $_GET['post']; }
-	//Check if the master field is selected
+	//Check if the master field is selected @jwaterer 
 	if( 1 == get_field('remove_others',$post_id) ) {
-		//Stores current course selected of the current post in admin.
+		//Stores current course selected of the current post in admin. @jwaterer 
 		$GLOBALS['current_course'] = get_field('course',$post_id)->ID;
-		//Check all of the current post type for course value
+		//Check all of the current post type for course value @jwaterer 
 	    $args = array('post_type' => $post_type,);
 	    $loop = new WP_Query($args);	
 
 	     if($loop->have_posts()) {
 	        while($loop->have_posts()) : $loop->the_post();
-	        	//Store the field in var to prevent slow server issues
+	        	//Store the field in var to prevent slow server issues @jwaterer 
 	        	$check = get_field('course',get_the_ID())->ID;
-	 			//checks if the current course on the page is matched on any other page.
+	 			//checks if the current course on the page is matched on any other page. @jwaterer 
 	        	if($GLOBALS['current_course'] == $check){ 
-	        	//Updates all the fields to 0 - no master
+	        	//Updates all the fields to 0 - no master @jwaterer 
 	        	update_field('remove_others',0,get_the_ID());
 	        	}
 	        endwhile;
 	     }
-	     	//Sets the master field back to true(1) for the current post
+	     	//Sets the master field back to true(1) for the current post @jwaterer 
 			update_field('remove_others',1,$post_id);
 			add_action( 'admin_notices', 'my_admin_notice' );
 		}
