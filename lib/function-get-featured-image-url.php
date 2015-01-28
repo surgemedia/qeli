a82ec1a24ef153b715bea6e614dcb3fb464228dd
Alex <alex.king@surgemedia.com.au>
2015-01-28 15:18:29 +1000

    Update - Get Featured Image Url
    added Default value to function for easy links on the fly

--- lib/function-get-featured-image-url.php
+++ lib/function-get-featured-image-url.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
 /*=========================================
 =         get featured image url            =
 =========================================*/
 //Call inside the loop
-function getFeaturedUrl(){
+
+function getFeaturedUrl($id = NULL){
+if(NULL != $id){
+	$thumb_id = get_post_thumbnail_id($id);
+} else {
 $thumb_id = get_post_thumbnail_id();
+}
 $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 $thumb_url = $thumb_url_array[0];
 return $thumb_url;
