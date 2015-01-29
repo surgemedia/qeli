d3f501059d2107cd6e9754c6163f40356e5a1e2e
Alex <alex.king@surgemedia.com.au>
2015-01-29 10:32:09 +1000

    Update - Course Template

--- template-courses.php
+++ template-courses.php
@@ -10,6 +10,15 @@ Template Name: Courses Page
 		<?php  get_template_part('templates/content', 'featured-program'); ?>
 		<?php get_template_part('templates/content', 'featured-Testimonial'); ?>
 	</div>
+	<div class="featured-image">
+    <div class="featured-text">
+      <div class="col-xs-12 col-sm-4 text-blue">
+        The Middle Leadership Program from QELi fast-tracked my development.
+        <br><a href="#" class="btn btn-default">More about this program</a>
+      </div>
+    </div>
+  </div>
+  
 	<?php endwhile; ?>
 	<?php  get_template_part('templates/template-post-loop', 'courses'); ?>
 </div>
