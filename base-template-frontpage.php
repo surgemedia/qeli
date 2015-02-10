56c852ffc992cc04213a902797d49d8975f6242c
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-10 17:03:32 +1000

    Misc

--- base-template-frontpage.php
+++ base-template-frontpage.php
@@ -21,13 +21,8 @@
   </div><!-- /.wrap -->
   </article>
   </div>
-  <div class="modal fade in" id="videoModal" >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="embed-responsive embed-responsive-16by9">
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php
+    get_template_part('templates/modal', 'video');
+  ?>
 </body>
 </html>
