4c23d796b591247548ff2a023ffdac18ee3d4d98
Alex <alex.king@surgemedia.com.au>
2015-02-10 11:12:05 +1000

    Update - New Design Video Page

--- template-qeli-talks.php
+++ template-qeli-talks.php
@@ -6,20 +6,40 @@ Template Name: Qeli Talks/Videos
 <?php while (have_posts()) : the_post(); ?>
 <article id="content" class="col-xs-12">
 	<div class="row">
+	<?php $featured_id = get_field('featured_video')->ID; ?>
+	<?php if(0 == strlen(get_field('featured_video'))) {?>
 		<div class="page-header colored-background image-background" style="background-image:url('<?php 
 			$id = get_post_thumbnail_id();
 			echo wp_get_attachment_image_src($id, 'full')[0];
 		?>')">
+		<?php } else { ?>
+		<div class="page-header colored-background image-background" style="background-image:url('<?php 
+			echo getFeaturedUrl($featured_id);
+		?>')">
+		<?php } ?>
 			<?php get_template_part('templates/page', 'colored-header'); ?>
 			<div class="container">
-				<p>
-				<?php the_content(); ?>
-				</p>
+			<?php if(0 != strlen(get_field('featured_video') )) {?>
+			<!-- @jakew Duplicate Title? -->
+			<h2><?php echo get_the_title($featured_id); ?></h2>
+		<div class="video-modal graphic btn-play-sm" data-modal="#videoModal" data-src="//www.youtube.com/embed/<?php echo cleanYoutubeLink(get_field('embed_code',$featured_id)); ?>">
+			<?php } else { ?>
+			<?php the_content(); ?>
+			<?php } ?>
 			</div>
 		</div>
 	</div>
+	<?php if(0 != strlen(get_field('featured_video') )) {?>
+	<div class="container">
+	<!-- @jakew Duplicate Title? -->
+		 <h2 class="video-name"><?php echo get_the_title($featured_id); ?></h2>
+		 <span class="date-time"><?php echo get_the_date('F j');  ?></span>
+		  <p><?php echo truncate(get_field('description',$featured_id),25,"..."); ?>
+	</div>
+	<?php } ?>
 	<div class="container">
 		<div class="container">
+		
 			<?php
 			// WP_Query arguments
 			$args = array (
--- templates/content-post-type-video.php
+++ templates/content-post-type-video.php
@@ -1,15 +1,4 @@
-<?php /* ?>
-<div class="video-item">
-	<h2><?php the_title(); ?></h2>
-	<?php
-	for ($i=0; $i < sizeof(get_field('talks')); $i++) { ?>
-	<div class="embed-responsive embed-responsive-16by9">
-		<iframe allowfullscreen="" frameborder="0" height="315" src="//www.youtube.com/embed/<?php echo cleanYoutubeLink(get_field('talks')[$i]['embed_code']); ?>" width="560"></iframe>
-	</div>
-	<?php	} ?>
-	
-</div>
-<?php */ ?>
+
 <div class=" col-xs-12 col-sm-6 col-md-6">
   <div class="video-item col-sm-4">
 	<img alt="400x225" class="img-responsive" src="<?php echo getFeaturedUrl(); ?>" data-holder-rendered="true">
@@ -17,11 +6,11 @@
 	</div>
 </div>
   <div class="col-xs-12 col-sm-7">
-    <h3 class="author-name"><?php the_title(); ?> / <?php echo get_the_date('F j');  ?></h3>
+    <h3 class="title"><?php the_title(); ?> / <?php echo get_the_date('F j');  ?></h3>
   </div>
   <div class="col-xs-7">
     <div class="item-text">
-      <p><?php echo truncate(get_field('talks')[0][description],25,"..."); ?>
+      <p><?php echo truncate(get_field('talks')[0]['description'],25,"..."); ?>
       </p>
     </div>
     <span class="share-link">Share</span>
