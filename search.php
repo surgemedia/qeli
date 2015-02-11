159beeeb485a3c1be2d915ed17f5b5b42a9da760
Alex <alex.king@surgemedia.com.au>
2015-02-11 14:52:59 +1000

    Update - Search Results

--- search.php
+++ search.php
@@ -3,16 +3,30 @@
 	Search Results Page
 */
 ?>
- <article id="content" class="col-xs-12">
+<article id="content" class="col-xs-12">
 	<div class="row">
-		<div class="page-header colored-background image-background" style="background-image:url('<?php 
-			$id = get_post_thumbnail_id();
-			echo wp_get_attachment_image_src($id, 'full')[0];
-			// TODO @walt showing bg of last result.
-		?>')">
-			<?php get_template_part('templates/page', 'colored-header'); ?>
+		<div class="page-header colored-background image-background" style="background-image:url('<?php
+					$id = get_post_thumbnail_id();
+					echo wp_get_attachment_image_src($id, 'full')[0];
+					// TODO @walt showing bg of last result.
+			?>')">
+			<div class="header">
+				<div class="container">
+					<h1 class="pagetitle">Search Result<?php /* Search Count */
+					$allsearch = &new WP_Query("s=$s&showposts=-1");
+					$key = wp_specialchars($s, 1);
+					$count = $allsearch->post_count;
+					?></h1>
+				</div>
+			</div>
 			<div class="container header-text">
-				<?php get_search_form(); ?>
+				<div class="col-lg-6">
+					<?php  get_search_form(); ?>
+				</div>
+				<p class="col-lg-6"><?php echo $count . ' '; _e('articles found for');
+				_e(' <span class="search-terms">');
+				echo $key; _e('</span> '); ?>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -20,15 +34,19 @@
 		<div class="row">
 			
 			<div class="col-xs-12 <?php if (get_field('aside')) { echo 'col-sm-8'; } ?>">
-		
-		<?php while (have_posts()) : the_post(); ?>
 				
-		<?php get_template_part('templates/content-post-type', 'media-release'); ?>
-		
-		<?php endwhile; ?>
-
+				<?php if(have_posts()) { ?>
+				<?php while (have_posts()) : the_post(); ?>
+				
+				<?php get_template_part('templates/content-post-type', 'media-release'); ?>
+				
+				<?php endwhile; ?>
+				<?php } else { ?>
+				<?php get_template_part('templates/content', 'no-posts'); ?>
+				
+				<?php wp_reset_query(); ?>
+				<?php } ?>
 			</div>
-
 			
 		</div>
 	</div>
