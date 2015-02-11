26b3e8bea62a0a43b91d1cb207f78554d8dedba7
Alex <alex.king@surgemedia.com.au>
2015-02-11 15:42:58 +1000

    Update - Media Release Error check

--- templates/content-post-type-media-release.php
+++ templates/content-post-type-media-release.php
@@ -1,9 +1,9 @@
 <?php 
+if( 0 <= strlen(get_field('attachment')['id']) ){
 $filesize_kb = filesize(get_attached_file(get_field('attachment')['id']));
 $filesize = size_format($filesize_kb, 2);
+};
  ?>
-
-
 <div class="media-item">
 	<h2><a href="<?php the_permalink(); ?>"><span class="graphic arrow-link"></span><?php the_title(); ?></a></h2>
 	<p>
