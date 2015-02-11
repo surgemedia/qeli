aabb1c28c8c6a4d9f5827379e01c9547af0157e4
Alex <alex.king@surgemedia.com.au>
2015-02-11 15:09:42 +1000

    Fix - has header text not working

--- templates/content-header-text.php
+++ templates/content-header-text.php
@@ -2,7 +2,9 @@
 	<div class="row">
 		<div class="col-xs-12 col-sm-8">
 			<?php if (get_field('header_text')): ?>
+			<?php if(false != get_field('has_header_text')) { ?>
 			<?php the_field('header_text'); ?>
+			<?php } ?>
 			<?php endif; ?>
 		</div>
 	</div>
