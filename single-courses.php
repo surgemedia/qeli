e6ed89bd7c78740f8be0d94dc3d5ffc6f9292ca9
Alex <alex.king@surgemedia.com.au>
2015-02-12 10:59:03 +1000

    Updated - Better Checks

--- single-courses.php
+++ single-courses.php
@@ -21,7 +21,7 @@
 					for ($i=0; $i < sizeof($instanes); $i++) {
 						if( 0 < strlen($instanes[$i]['facilitator'])){
 						$facil = $instanes[$i]['facilitator'];
-						if(strpos(',',$facil)){
+						if(0 < strpos(',',$facil)){
 						$facil_array = explode(',', $facil);
 							} else {
 								$facil_array = array($facil);
