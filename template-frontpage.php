fb37fdbf91f287c0b9c618956c0c6e7977b14709
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-12 15:33:29 +1000

    PHP LESS Footer + Homepage + Video Section

--- template-frontpage.php
+++ template-frontpage.php
@@ -5,20 +5,20 @@ Template Name: Home Page
 ?>
 <?php while (have_posts()) : the_post(); ?>
 <ul>
-<?php 
+<?php
 $repeater = get_field('template_selector');
-for ($i=0; $i < sizeof($repeater); $i++) { 
+for ($i=0; $i < sizeof($repeater); $i++) {
 	echo "<li>";
 	get_template_part('templates/template-block',$repeater[$i]['template_blocks']);
 	echo "</li>";
 }
 ?>
-	
+
 	<li>
-		<div class="section" >
+		<div class="section home-footer" >
 			<?php get_template_part('templates/footer'); ?>
-			<?php wp_footer(); ?>
 		</div>
 	</li>
 </ul>
 <?php endwhile; ?>
+<?php wp_footer(); ?>
--- templates/footer.php
+++ templates/footer.php
@@ -23,49 +23,6 @@
                             );
                     ?>
         </div>
-        <!--
-        <div class="row">
-            <div class="col-xs-6 col-md-3">
-                <h2><span class="graphic footer-qeli"></span>QELI</h2>
-                <ul>
-                    <li><a href="#">Contact us</a></li>
-                    <li><a href="#">Privacy</a></li>
-                    <li><a href="#">Terms of use</a></li>
-                    <li><a href="#">Disclaimer</a></li>
-                    <li><a href="#">Sitemap</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-6 col-md-3">
-
-                <h2><span class="graphic footer-about"></span>About us</h2>
-                <ul>
-                    <li><a href="#">CEO and Board</a></li>
-                    <li><a href="#">Ambassadors</a></li>
-                    <li><a href="#">Results and Outcomes</a></li>
-                    <li><a href="#">Publications</a></li>
-                    <li><a href="#">Contact QELi</a></li>
-                </ul>
-            </div>
-            <div class="clearfix visible-xs-block"></div>
-            <div class="col-xs-6 col-md-3">
-                <h2><span class="graphic footer-courses"></span>Our programs</h2>
-                <ul>
-                    <li><a href="#">Programs overview</a></li>
-                    <li><a href="#">Course example</a></li>
-                    <li><a href="#">Course cart</a></li>
-                </ul>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <h2><span class="graphic footer-news"></span>News/Resources</h2>
-                <ul>
-                    <li><a href="#">Newsletters</a></li>
-                    <li><a href="#">Media Releases</a></li>
-                    <li><a href="#">QELi Talks</a></li>
-                    <li><a href="#">Sector News</a></li>
-                </ul>
-            </div>
-        </div>
-        -->
     </div>
     <div class="copyright">
         <div class="container">
