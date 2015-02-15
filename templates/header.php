926ea898d86de9be5c46eda853bc46afdae840bc
Alex <alex.king@surgemedia.com.au>
2015-02-16 09:33:38 +1000

    Update - Included form instead of button
    Newsletter Header

--- templates/header.php
+++ templates/header.php
@@ -45,24 +45,21 @@
 						<div id="subscribe-collapse" class="panel subscribe-form collapse">
 							<div class="panel-heading">
 								<h3>Subscribe</h3><span class="graphic icon-close" data-target="#subscribe-collapse" data-toggle="collapse" aria-expanded="false" aria-controls="contact-collapse"></span>
-								<div class="createsend-button" style="height:22px;display:inline-block;" data-listid="d/D9/4D5/D45/12304556F24E0BC8"></div>
-<script type="text/javascript">(function () { var e = document.createElement('script'); e.type = 'text/javascript'; e.async = true; e.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://btn.createsend1.com/js/sb.min.js?v=3'; e.className = 'createsend-script'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s); })();</script>
 
-</div>
 							</div>
-							<?php /* Removed for subscription popup ?>
+							
 							<div class="panel-body">
-								<form role="subscribe" method="get" class="submit-form form-inline" action="<?php echo esc_url(home_url('/')); ?>">
+								<form action="http://qeli.createsend.com/t/d/s/uiklt/" method="post">
 								  <label class="sr-only"><?php _e('Subscribe', 'roots'); ?></label>
 								  <div class="input-group">
-								    <input type="subscribe" name="s" class="subscribe-field form-control" placeholder="email" required>
+								   <input id="fieldEmail" name="cm-uiklt-uiklt" type="email" required />
 								    <span class="input-group-btn">
 								      <button type="submit" class="search-submit btn btn-subscribe"><?php _e('Subscribe', 'roots'); ?></button>
 								    </span>
 								  </div>
 								</form>
 							</div>
-							*/ ?>
+							
 
 							
 						
