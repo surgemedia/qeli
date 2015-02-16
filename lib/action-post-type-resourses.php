c48b4f1a32cb222f693d947369c1f01eb60c43a0
Alex <alex.king@surgemedia.com.au>
2015-02-16 14:13:28 +1000

    BH157 - New - Taxonomy for Resources

--- lib/action-post-type-resourses.php
+++ lib/action-post-type-resourses.php
@@ -1,4 +1,39 @@
 <?php 
+// Register Custom Taxonomy
+function Type_report() {
+
+	$labels = array(
+		'name'                       => _x( 'Types', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Type', 'text_domain' ),
+		'all_items'                  => __( 'All Items', 'text_domain' ),
+		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+		'new_item_name'              => __( 'New Item Type', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Type', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Item', 'text_domain' ),
+		'update_item'                => __( 'Update Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+		'search_items'               => __( 'Search Items', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'type', array( 'resources' ), $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'Type_report', 0 );
 // Register Custom Post Type
 function resources() {
 
@@ -22,7 +57,7 @@ function resources() {
 		'description'         => __( 'resources', 'text_domain' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'excerpt', 'thumbnail', ),
-		'taxonomies'          => array( 'Type' ),
+		'taxonomies'          => array( 'type' ),
 		'hierarchical'        => false,
 		'public'              => true,
 		'show_ui'             => true,
