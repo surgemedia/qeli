fe397b9150eaa931326e75ec7d0a18f3b3d41a1a
Alex <alex.king@surgemedia.com.au>
2015-02-16 13:55:55 +1000

    Update - Added correct links to title link

--- templates/content-post-type-media-release.php
+++ templates/content-post-type-media-release.php
@@ -4,12 +4,16 @@ if( get_field('attachment') ){
 	$filesize_kb = filesize(get_attached_file(get_field('attachment')['id']));
 	$filesize = size_format($filesize_kb, 2);
 };
- ?>
+?>
 <div class="media-item">
+	<?php if(!isset( get_field('attachment')['url'] ) ) { ?>
 	<h2><a href="<?php the_permalink(); ?>"><span class="graphic arrow-link"></span><?php the_title(); ?></a></h2>
+	<?php } else { ?>
+	<h2><a href="<?php echo get_field('attachment')['url'] ?>"><span class="graphic arrow-link"></span><?php the_title(); ?></a></h2>
+	<?php } ?>
 	<p>
-		<?php the_excerpt();
-		?>
+	<?php the_excerpt();
+	?>
 	</p>
 	<?php if(!isset( get_field('attachment')['url'] ) ) { ?>
 	<a href="<?php the_permalink(); ?>" class="media-link"><span class="graphic icon-read-more"></span>Read more</a>
