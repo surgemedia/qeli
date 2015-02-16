4e661a03e811ac1f0abd914eb29eb6a269611719
Alex <alex.king@surgemedia.com.au>
2015-02-17 09:47:07 +1000

    Cleaning - Removed close div in part file

--- template-ceo-board.php
+++ template-ceo-board.php
@@ -7,42 +7,37 @@ Template Name: Ceo and Board
 <div class="person ceo">
 	<div class="page-header colored-background">
 		<?php get_template_part('templates/page', 'colored-header'); ?>
-
-
-
 		<?php
-	//Gets a templated post from the ID
-	$args = array (
-	'post_type'                 => 'key_people',
-	'p'                      => get_field('ceo'),
-	);
-
-	$query = new WP_Query( $args );
-
-	if ( $query->have_posts() ) {
+		//Gets a templated post from the ID
+		$args = array (
+		'post_type'                 => 'key_people',
+		'p'                      => get_field('ceo'),
+		);
+		$query = new WP_Query( $args );
+		if ( $query->have_posts() ) {
 		while ( $query->have_posts() ) {
-		$query->the_post(); 
-			get_template_part('templates/content-page', 'acf-ceo'); 
-		}
-	}
-	else {
-		get_template_part('templates/content', 'no-posts');
-	}
-	// Restore original Post Data
-	wp_reset_postdata()
-?>
-</div>
-<hr>
+		$query->the_post();
+			get_template_part('templates/content-page', 'acf-ceo-short');
+			echo "</div>";
 
-<div class="container">
-	<?php for ($i=0; $i < sizeof(get_field('people_repeater')); $i++) {
-		$GLOBALS['people_id'] = get_field('people_repeater')[$i]['board_member']->ID;
-		get_template_part('templates/content-page', 'acf-normal');
-
-		if (($i + 1)%2 == 0) {
-			echo '<div class="clearfix"></div>';
-		}
-	} //For Loop ?>
-</div>
-
-<?php endwhile; ?>
+			get_template_part('templates/content-page', 'acf-ceo');
+			}
+			}
+			else {
+			get_template_part('templates/content', 'no-posts');
+			}
+			// Restore original Post Data
+			wp_reset_postdata()
+			?>
+		</div>
+		<hr>
+		<div class="container">
+			<?php for ($i=0; $i < sizeof(get_field('people_repeater')); $i++) {
+				$GLOBALS['people_id'] = get_field('people_repeater')[$i]['board_member']->ID;
+				get_template_part('templates/content-page', 'acf-normal');
+				if (($i + 1)%2 == 0) {
+					echo '<div class="clearfix"></div>';
+				}
+			} //For Loop ?>
+		</div>
+		<?php endwhile; ?>
