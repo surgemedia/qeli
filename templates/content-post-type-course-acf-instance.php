29eff4aa43cb0515a866bc4a8f02e3c624148f06
Alex <alex.king@surgemedia.com.au>
2015-02-16 16:29:22 +1000

    BH72 Fixed - Instance naming issue

--- templates/content-post-type-course-acf-instance.php
+++ templates/content-post-type-course-acf-instance.php
@@ -28,14 +28,14 @@ $programinstance_type = 'Scheduled Instance';
   <div id="collapse<?Php echo $GLOBALS['instance_count'] ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?Php echo $GLOBALS['instance_count'] ?>">
     <div class="panel-body">
       <p>
-      <?php echo get_field('instances')[$GLOBALS['instance_count']]['venue_name'] ?>,<br>
-      <?php echo get_field('instances')[$GLOBALS['instance_count']]['venue_address'] ?>
+      <?php //echo get_field('instances')[$GLOBALS['instance_count']]['venue_name'] ?><!-- ,<br> -->
+      <?php //echo get_field('instances')[$GLOBALS['instance_count']]['venue_address'] ?>
       </p>
       <ul>
         <?php
         $phases = get_field('instances')[$GLOBALS['instance_count']]['phases'];
         for ($i=0; $i < sizeof($phases); $i++) { ?>
-        <li><?php echo $phases[$i]['name']; ?> - <?php echo $phases[$i]['type']; ?> <?php echo $phases[$i]['start']; ?> to <?php echo $phases[$i]['end']; ?></li>
+        <li><?php echo $phases[$i]['name']; ?> - <?php echo $phases[$i]['type']; ?> <?php echo $phases[$i][$GLOBALS['instance_count']]['city']; ?></li>
         <?php  } ?>
         
         
