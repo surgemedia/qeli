2558d911479446c8442e12db57a303685f2ae3bf
Alex <alex.king@surgemedia.com.au>
2015-02-17 10:26:50 +1000

    Cleaning - Import brough through p tags

--- templates/content-post-type-courses.php
+++ templates/content-post-type-courses.php
@@ -1,26 +1,19 @@
+<?php //  ?>
 <div class="course col-xs-12 col-sm-6" 
-    data-audience="<?php the_field('audience'); ?>"
-    data-fee="<?php the_field('cost'); ?>"
-    data-location="<?php the_field('instances')[0]['state']; ?>"
-    data-delivery="<?php
-                $terms = get_field('delivery_method');
-                for ($i=0; $i < sizeof($terms); $i++) {
-                  if($i == 0) {
-                    echo $terms[$i]->name;
-                  }
-                  else {
-                    echo ", ".$terms[$i]->name;
-                  }
-                } 
-              ?>" 
+    data-audience="<?php echo strip_tags(get_field('audience')); ?>"
+    data-fee="<?php echo strip_tags(get_field('cost')); ?>"
+    data-location="<?php echo strip_tags(get_field('instances')[0]['location']); ?>"
+    data-delivery="<?php echo strip_tags(get_field('deliveryMethod')); ?>" 
+    <?php /* Removed from Course - Alex  ?>
     data-month="<?php echo get_field('instances')[0]['date']; ?>" 
     data-development="<?php the_field('development'); ?>" 
+    <?php */ ?>
     >
     <div class="row">
     <div class="col-xs-12">   
       <h2><a href="<?php the_permalink() ?> "><?php the_title(); ?></a></h2>
       <!--
-      <a href="<?php echo site_url(); ?>/cart/" class="graphic icon-cart pull-right"></a>
+      <a href="<?php //echo site_url(); ?>/cart/" class="graphic icon-cart pull-right"></a>
       <span class="sep pull-right"></span>
       -->
       <span class="graphic icon-star pull-right"></span>
@@ -52,11 +45,12 @@
             <td><b>Location</b></td>
             <td><?php echo get_field('locations'); ?></td>
           </tr>
-          <tr>
+          <?php /*  Removed from Course - Alex ?>
+          <tr> 
             <td><b>Month</b></td>
             <td><?php echo get_field('instances')[0]['instances_name']; ?></td>
           </tr>
-          <?php /* ?>
+          
           <tr>
             <td><b>Development</b></td>
             <td><?php truncate(get_field('outcome'),9,"..."); ?></td>
