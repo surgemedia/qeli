e60d24e2acc453ca382154b3d4dbeb901e569fef
Alex <alex.king@surgemedia.com.au>
2015-02-20 17:25:49 +1000

    Update - Edit links and Bio

--- templates/content-post-type-people.php
+++ templates/content-post-type-people.php
@@ -2,7 +2,7 @@
 									 ?>
 <div class="person col-xs-12 col-sm-6 col-md-4"
 	data-title="<?php the_title();?>"
-	data-description="<?php echo get_field('short_description');?>"
+	data-description="<?php echo get_field('biography');?>"
 	data-lastname="<?php echo strip_tags(get_field('last_name')); ?>"
 	data-term="<?php $terms = wp_get_post_terms($post->ID, 'people_group');
 					foreach($terms as $term) {echo $term->name . ' ';  }
@@ -15,18 +15,20 @@
 						 data-src="<?php echo getFeaturedUrl(); ?>" alt="150x150"
 						 data-holder-rendered="true"/>
 					<div class="graphic panel-info-callout"></div>
+
 				</div>
 			</div>
 			<div class="col-xs-6">
 				<div class="person-info">
 					<h3><?php the_title(); ?></h3>
-					<!--
+					<?php /* ?>
 					<?php if (the_field('author_role')): ?>
 						<?php the_field('author_role'); ?>
 						<br>
 					<?php endif; ?>
-					-->
-					VIEW BIO
+					*/ ?>
+					<span>VIEW BIO</span>
+					<span><?php edit_post_link(); ?></span>
 				</div>
 			</div>
 		</div>
