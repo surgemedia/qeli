b20e452114378814c34c288f02504ae209472b2f
Alex <alex.king@surgemedia.com.au>
2015-02-20 15:13:47 +1000

    Update - Linked up Homepage Videos
    @jwaterer still an issue with the js, not calling the attached video.

--- templates/template-block-big-video.php
+++ templates/template-block-big-video.php
@@ -1,55 +1,43 @@
-
 <div class="section videos blue alt">
   <div class="video-top-panel">
     <div class="container">
       <div class="col-xs-12">
         <h2>QEL<span class="text-lowercase">i</span>: Be the difference, that makes a difference, to the lives of children</h2>
       </div>
-  </div>
-  </div>
-
-      <?php
-          // TODO JW - this needs to be seperated into a query that returns the main video (brand video) and then these others.
-          // WP_Query arguments
-      $count = 0;
-      $args = array (
-        'post_type'              => 'videoes',
-        'post_count'            => 1,
-        'posts_per_page'        => 1,
-        );
-          // The Query
-      $query = new WP_Query( $args );
-          // The Loop
-      if ( $query->have_posts() ) :
-        while ( $query->have_posts() ) : $query->the_post();  ?>
-      <?php if(0 == $count) : ?>
-      <?php // checks if 1st post, then counts ?>
-
-
-<div class="video-item max-height">
-  <div class="embed-responsive embed-responsive-16by9">
-    <div id="player" class="max-height" video-id="<?php echo cleanYoutubeLink(get_field('talks')[0]['embed_code']); ?>"></div>
-  <!-- <iframe id="main-video" width="1280" height="720" src="//www.youtube.com/v/<?php echo cleanYoutubeLink(get_field('talks')[0]['embed_code']); ?>?enablejsapi=1&version=3&playerapiid=ytplayer" frameborder="0" allowfullscreen></iframe> -->
-</div>
-  <div class="overlay video-in-situ" style="background-image:url('http://54.79.72.151/~qeliedu/wp-content/uploads/2015/02/kid4.png')">
-      <span class="video-modal graphic btn-play-xl"></span>
-  </div>
-</div>
-  <?php endif; ?>
-<?php  endwhile; ?>
-<?php  else :
-      //Sorry No Post Yet
-endif;
-      // Restore original Post Data
-wp_reset_postdata()
-?>
-<div class="video-bottom-panel">
-  <div class="container">
-    <div class="col-xs-12">
-      <h3 class="video-title">Video title</h3>
-      <p class="video-date">Video Date</p>
-      <a href="<?php echo site_url(); ?>/program-catalogue" class="big-link"><span class="graphic arrow-link-sq"></span> See our program catalogue</a>
     </div>
   </div>
-</div
+  <?php
+  $args = array (
+  'post_type'              => 'videoes',
+  'posts_per_page'        => 1,
+  'meta_query'    => array(
+  
+  array( 'key' => 'featured', 'value'     => 1,))
+  );
+  // The Query
+  $query = new WP_Query( $args );
+  // The Loop
+  if ( $query->have_posts() ) {
+  while ( $query->have_posts() ) : $query->the_post();
+  $image_url = getFeaturedUrl(get_the_id(),'full');
+  ?>
+  <div class="video-item max-height">
+    <div class="embed-responsive embed-responsive-16by9">
+      <div id="player" class="max-height" video-id="<?php echo cleanYoutubeLink(get_field('talks')[0]['embed_code']); ?>"></div>
+    </div>
+    <div class="overlay video-in-situ" style="background-image:url('<?php echo $image_url; ?>')">
+      <span class="video-modal graphic btn-play-xl"></span>
+    </div>
+  </div>
+  <div class="video-bottom-panel">
+    <div class="container">
+      <div class="col-xs-12">
+        <h3 class="video-title"><?php the_title( ); ?></h3>
+        <p class="video-date"><?php echo get_the_date('F j'); ?></p>
+        <a href="<?php echo site_url(); ?>/program-catalogue" class="big-link"><span class="graphic arrow-link-sq"></span> See our program catalogue</a>
+      </div>
+    </div>
+  </div
 </div>
+<?php endwhile; //while ?>
+<?php } //if have posts ?>
--- templates/template-block-posts-video.php
+++ templates/template-block-posts-video.php
@@ -5,28 +5,49 @@
       // TODO JW - this needs to be seperated into a query that returns the main video (brand video) and then these others.
       // WP_Query arguments
       $count = 0;
+      $featured_video;
       $args = array (
       'post_type'              => 'videoes',
-
+      'posts_per_page'        => 1,
+      'meta_query'    => array(
+      
+      array( 'key' => 'featured', 'value'     => 1,))
       );
       // The Query
       $query = new WP_Query( $args );
-
       // The Loop
       if ( $query->have_posts() ) {
       while ( $query->have_posts() ) : $query->the_post();  ?>
       <?php  $featured = get_field('featured',get_the_id()); ?>
-
-      <?php if($featured == 1 && $count == 0) { ?>
-      <?php  get_template_part('templates/content-post-type-post-block', 'big-video');  $count++; $featured = 0;?>
-      <?php } else {
-         get_template_part('templates/content', 'no-posts');
-        } ?>
-       <?php endwhile; //while ?>
-     <?php } //if have posts ?>
-
+      
+      <?php  $featured_video = get_the_id(); ?>
+      <?php  get_template_part('templates/content-post-type-post-block', 'big-video');  $count = 1; $featured = 0;?>
+      
+      <?php endwhile; //while ?>
+      <?php } //if have posts ?>
+      <?php
+      
+      $args = array (
+      'post_type'              => 'videoes',
+      'post_count'            => 4,
+      'posts_per_page'        => -1,
+      );
+      // The Query
+      $query = new WP_Query( $args );
+      // The Loop
+      if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) : $query->the_post();  ?>
+      <?php  $featured = get_field('featured',get_the_id()); ?>
+    </div>
+    <div class="col-xs-12 col-md-3 row">
+      <div class="col-xs-12 col-sm-4 col-md-12">
+        <?php if(get_the_id() != $featured_video) { ?>
+        <?php get_template_part('templates/content-post-type-post-block', 'video');?>
+        <?php } ?>
+      </div>
+      <?php endwhile; //while ?>
+      <?php } //if have posts ?>
       <?php wp_reset_postdata(); ?>
-
     </div>
   </div>
   <div class="section-footer">
