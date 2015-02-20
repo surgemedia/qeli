db2c279c1853ed9197c6cd30f91d8f98402765b3
Alex <alex.king@surgemedia.com.au>
2015-02-20 17:25:33 +1000

    Update - Show info on eco board

--- templates/content-page-acf-normal.php
+++ templates/content-page-acf-normal.php
@@ -2,14 +2,15 @@
 	<div class="person">
 		<img src="<?php echo getFeaturedUrl($GLOBALS['people_id']) ?>" class="img-responsive img-circle" alt="<?php the_field('name') ?>">
 		<h2><?php echo get_the_title($GLOBALS['people_id']) ?></h2>
-		<?php if (get_field('qualifications')): ?>
+		<?php if (get_field('qualifications',$GLOBALS['people_id'])): ?>
 			<div class="meta-qualifications"><?php echo get_field('qualifications',$GLOBALS['people_id']) ?></div>
 		<?php endif; ?>
-
-		<div class="meta-title h3 colored-text">Board Chair</div>
+		<?php if(get_field('author_role',$GLOBALS['people_id'])) { ?>
+		<div class="meta-title h3 colored-text"><?php echo get_field('author_role',$GLOBALS['people_id']); ?></div>
+		<?php } ?>
 		<?php echo get_field('biography',$GLOBALS['people_id']) ?>
 
-		<?php if (get_field('special_responsibilities')): ?>
+		<?php if (get_field('special_responsibilities',$GLOBALS['people_id'])): ?>
 			<div class="meta-special">
 				<h3>Special Responsibilities:</h3>
 				<?php echo get_field('special_responsibilities',$GLOBALS['people_id']) ?>
