7bd53802fa8a2c03a2910cf4f1dfd6f6df25e7ec
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-23 13:28:06 +1000

    Programs page alternative

---
+++ template-programs-page.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Template Name: Programs Page
+*/
+?>
+<?php while (have_posts()) : the_post(); ?>
+	<article id="content" class="col-xs-12">
+		<div class="row">
+			<div class="page-header colored-background image-background" style="background-image:url('http://54.79.72.151/~qeliedu/wp-content/uploads/2015/02/hero-image9.jpg')">
+				<div class="header">
+					<div class="container">
+						<h1 class="text-uppercase">Our programs</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="lead">
+						<p>Our exceptional leadership development programs are driven by our passion for improving education in Queensland, nationally and internationally.</p>
+					</div>
+					<p>In today’s fast-paced and ever-changing educational landscape, educational leaders must be motivated, responsive and high performing. As an educational leader, one of the most important tasks you will undertake is choosing to invest in yourself and your team.</p>
+					<p>QELi can provide you and your team with a range of leadership programs and solutions that connect you to eminent industry leaders, can be tailored to suit your context, grounded in up-to-the minute research, fitting in with your team’s schedule and budget. Whether you are a teacher leader, middle leader, master teacher, aspiring principal, senior executive or system leader, QELi has a range of quality programs and courses that can be tailored to suit your unique experiences and aspirations.</p>
+					<div class="lead">
+						<p>Our offering includes three different kinds of programs:</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Schdeuled Programs</h3>
+						</div>
+						<div class="panel-body">
+							<p>QELi runs a selection of scheduled programs which you can enrol in directly on this site. </p>
+							<p>If you know the name of your course then please select it here:</p>
+							<select>
+								<option> - Course Name - </option>
+								<option>Course 1</option>
+								<option>Course 2</option>
+								<option>Course 3</option>
+								<option>Course 4</option>
+								<option>Course 5</option>
+							</select> <a href="http://54.79.72.151/~qeliedu/courses/resource-management-for-educational-leaders/" class="btn btn-primary">Go</a>
+							<br>
+							<br>
+							<p>Or visit the Programs Catalogue for more options to search by</p>
+							<a href="http://54.79.72.151/~qeliedu/program-catalogue/" class="btn btn-primary">Program Catalogue</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Custom Programs</h3>
+						</div>
+						<div class="panel-body">
+							<p>We’ll work closely with you to understand your situation, requirements and goals. We can then customise and contextualise one of our existing programs, or create an integrated leadership development solution specifically for you. QELi’s customised programs can be delivered on-site and within budget.</p>
+							<br>
+							<br>
+							<a href="http://54.79.72.151/~qeliedu/customised-programs/" class="btn btn-primary">Read more</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Specialist Services</h3>
+						</div>
+						<div class="panel-body">
+							<p>QELi can fully customise leadership programs for schools, systems and sectors across Australia and internationally. Our approach, program design and delivery can be tailored to the specific needs of your context and school setting.</p>
+							<br>
+							<br>
+							<br>
+							<a href="http://54.79.72.151/~qeliedu/specialist-services/" class="btn btn-primary">Read more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</article>
+<?php endwhile; ?>
