53fac588c3127e5c7fd47b1250d34ed523e1a2d9
Alex <alex.king@surgemedia.com.au>
2015-02-23 10:35:14 +1000

    BH228 logolink to homepage

--- templates/header.php
+++ templates/header.php
@@ -9,7 +9,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand graphic logo" href="#"></a>
+				<a class="navbar-brand graphic logo" href="<?php echo site_url(); ?>"></a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<?php
