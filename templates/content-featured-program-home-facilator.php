fdf5b806a54e42ab6bb6bf2bfdad6637cf3703a2
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-23 13:28:19 +1000

    Faetured program - minor style tweaks

--- templates/content-featured-program-home-facilator.php
+++ templates/content-featured-program-home-facilator.php
@@ -3,21 +3,21 @@ $fac_ids = get_field('instances')[0]['facilitator'];
 if($fac_ids){
 $facil_array = false;
 					//defined out of loop scope
-					
+
 					//debug( get_post_meta( get_the_ID() ));
-			
-						
-						
+
+
+
 						if(-1 != strpos(',',$fac_ids)){
-						$facil_array = explode(',', $fac_ids)[0]; 
+						$facil_array = explode(',', $fac_ids)[0];
 						//debug($facil_array);
 							} else {
 								$facil_array = array($fac_ids);
 							}
 						$fac_post = get_post($facil_array);
 						//debug($fac_post);
-						
-						
+
+
 					 ?>
 <div class="col-xs-12 col-sm-6 feature-facilitator">
 	<img class="img-circle img-responsive" src="<?php echo  getFeaturedUrl($facil_array) ?>"><br>
