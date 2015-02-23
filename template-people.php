e9953b1280782231f3340468ddb8e04e164632d5
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-23 14:53:37 +1000

    Our Community - People sort

--- template-people.php
+++ template-people.php
@@ -4,87 +4,73 @@ Template Name: About - People
 */
 ?>
 <?php while (have_posts()) : the_post(); ?>
-<article id="content" class="col-xs-12">
-	<div class="row">
-		<div class="page-header colored-background">
-			<?php get_template_part('templates/page', 'colored-header'); ?>
-			<div class="container">
-				<p>
-				<?php the_content(); ?>
-				</p>
+	<article id="content" class="col-xs-12">
+		<div class="row">
+			<div class="page-header colored-background">
+				<?php get_template_part('templates/page', 'colored-header'); ?>
+				<div class="container">
+					<p>
+						<?php the_content(); ?>
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div id="panel-info" class="col-xs-12 panel-info">
-		<div class="panel-header clearfix">
-			<button class="graphic icon-close pull-right"></button>
-			<h2 class="panel-title pull-left"></h2>
+		<div id="panel-info" class="col-xs-12 panel-info">
+			<div class="panel-header clearfix">
+				<button class="graphic icon-close pull-right"></button>
+				<h2 class="panel-title pull-left"></h2>
+			</div>
+			<div class="panel-text clearfix"></div>
 		</div>
-		<div class="panel-text clearfix"></div>
-	</div>
 
-	<div class="row">
-		<div class="container">
-			<div class="col-sm-2">
-				<div class="row">
+		<div class="row">
+			<div class="container">
+				<div class="col-sm-2">
+					<div class="row">
 
-					<ul class="people-filter">
-						<li><a href="#" class="selected" data-term="clear">All Staff</a></li>
-						<?php
+						<ul class="people-filter">
+							<li><a href="#" class="selected" data-term="clear">All Staff</a></li>
+							<?php
 							$args = array( 'hide_empty=0' );
 							$terms = get_terms( 'people_group', $args );
 							if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-							$count = count( $terms );
-							$i = 0;
-							$term_list = "";
+								$count = count( $terms );
+								$i = 0;
+								$term_list = "";
 								foreach ( $terms as $term ) {
-								$i++;
-								$term_list .= '<li><a data-term="' . $term->name . '" href="' . get_term_link( $term ) . '" title="' . sprintf( __( 'View all post filed under %s', 'my_localization_domain' ), $term->name ) . '">' . $term->name . '</a></li>';
-								if ( $count != $i ) {
+									$i++;
+									$term_list .= '<li><a data-term="' . $term->name . '" href="' . get_term_link( $term ) . '" title="' . sprintf( __( 'View all post filed under %s', 'my_localization_domain' ), $term->name ) . '">' . $term->name . '</a></li>';
+									if ( $count != $i ) {
 
+									}
+									else {
+
+									}
 								}
-								else {
+								echo $term_list;
+							}?>
 
-								}
-							}
-							echo $term_list;
-						}?>
-
-					</ul>
-					<hr>
-					<ul class="people-nav">
-						<?php
-
-						/*
-							$acf_people = get_field('key_people');
-
-						 	for ($i=0; $i < sizeof($acf_people); $i++) { ?>
-							 <?php
-							 	$acf_person = get_post(get_field('key_people')[$i]);
-							 	$acf_person_link = get_permalink(get_field('key_people')[$i]); ;
-							?>
-							<li><a href="<?php echo $acf_person_link; ?>" class="person-control" data-title="<?php echo $acf_person->post_title; ?>" data-item="person-<?php echo $i+1; ?> "><?php echo $acf_person->post_title;  ?></a></li>
-
-						<?php } */?>
+						</ul>
+						<hr>
+						<ul class="people-nav">
 
 						<?php // WP_Query arguments
-							$args = array (
+						$args = array (
 							'post_type'              => 'key_people',
-							
 							'pagination'             => false,
 							'posts_per_page'         => '-1',
-							'orderby'		=> 'meta_value_num',
+							'orderby'		=> 'meta_value',
 							'meta_key'		=> 'last_name',
-							'order'			=> 'DESC'
-							
+							'order'			=> 'ASC'
+
 							);
 							// The Query
-							$query = new WP_Query( $args );
+						$query = new WP_Query( $args );
 
-							if ( $query->have_posts() ): ?>
-								<?php while ( $query->have_posts() ): ?>
-									<?php $query->the_post(); ?>
+						if ( $query->have_posts() ): ?>
+						<?php while ( $query->have_posts() ): ?>
+						<?php $query->the_post(); ?>
 									<?php //Used to add base value of last name to our commnunity
 									//$new_name = explode(" ",get_the_title());
 									//$size = sizeof($new_name)-1;
@@ -94,34 +80,33 @@ Template Name: About - People
 									<?php wp_reset_postdata(); ?>
 
 								<?php endwhile; ?>
-						<?php endif; ?>
-					</ul>
+							<?php endif; ?>
+						</ul>
+					</div>
 				</div>
-			</div>
-			<div class="col-sm-10">
+				<div class="col-sm-10">
 				<?php // WP_Query arguments
-					$args = array (
+				$args = array (
 					'post_type'              => 'key_people',
-					
 					'pagination'             => false,
 					'posts_per_page'         => '-1',
-							'orderby'		=> 'meta_value_num',
-							'meta_key'		=> 'last_name',
-							'order'			=> 'DESC'
+					'orderby'		=> 'meta_value',
+					'order'			=> 'ASC',
+					'meta_key'		=> 'last_name'
 					);
 					// The Query
-					$query = new WP_Query( $args );
-							if ( $query->have_posts() ) {
-							while ( $query->have_posts() ) {
-							$query->the_post();
+				$query = new WP_Query( $args );
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) {
+						$query->the_post();
 
-							get_template_part('templates/content-post-type','people');
-							}
-							} else {
-							get_template_part('templates/content', 'no-posts');
-							}
+						get_template_part('templates/content-post-type','people');
+					}
+				} else {
+					get_template_part('templates/content', 'no-posts');
+				}
 							// Restore original Post Data
-							wp_reset_postdata();
+				wp_reset_postdata();
 							// WP_Query arguments
 				?>
 			</div>
