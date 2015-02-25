a9dc086389e7e86a4260f89a325a7e7045604f45
Alex <alex.king@surgemedia.com.au>
2015-02-25 14:59:20 +1000

    New - 404 Page

--- 404.php
+++ 404.php
@@ -1,13 +1,30 @@
-<?php get_template_part('templates/page', 'header'); ?>
+ <article id="content" class="col-xs-12">
+	<div class="row">
+		<div class="page-header colored-background image-background" style="background-image:url('<?php
+			$id = get_post_thumbnail_id();
+			echo wp_get_attachment_image_src($id, 'full')[0];
+		?>')">
+			<?php get_template_part('templates/page', 'colored-header'); ?>
 
-<div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
-</div>
-
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
+		<div class="container">
+  
 <ul>
   <li><?php _e('a mistyped address', 'roots'); ?></li>
   <li><?php _e('an out-of-date link', 'roots'); ?></li>
 </ul>
 
 <?php get_search_form(); ?>
+</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<?php get_template_part('templates/content', 'lead'); ?>
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
+</article>
+
