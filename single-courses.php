c2cb60ff324dead59ab11ef61a500d9cfaaa39ac
Alex <alex.king@surgemedia.com.au>
2015-02-26 16:12:37 +1000

    Fix - Facilators not working on courses

--- single-courses.php
+++ single-courses.php
@@ -1,33 +1,36 @@
 <?php
 				$course_id = get_the_ID();
 				$instanes = get_field('instances');
-				$facil_array = false;
+				$facil_array = array();
 				$suffix = ", ";
+				// Get the ids in an array
 				for ($i=0; $i < sizeof($instanes); $i++) {
-					if( 0 < strlen($instanes[$i]['facilitator'])){
-						$facil = $instanes[$i]['facilitator'];
-						if(-1 != strpos(',',$facil)){
-							$facil_array = explode(',', $facil);
+						$facil = trim($instanes[$i]['facilitator']);
+						debug($facil );
+						if(0 < strlen($facil)){
+						if(false === strstr($facil,',')){
+							array_push($facil_array, trim($facil));
 						} else {
-							$facil_array = array($facil);
-							
+							$facil_single = explode(',', $facil);
+							//debug($facil_single);
+							for ($j=0; $j < sizeof($facil_single); $j++) {
+								array_push($facil_array, trim($facil_single[$j]));
+								
+							}
 						}
 					}
-					//slit into array
+					
 				}
-
+				// Get the name for at a glance
 				if(false != $facil_array){
-				//use array of facilactors to get template
-					for ($i=0; $i < sizeof($facil_array); $i++) {
-						$GLOBALS['facilitator'] = $facil_array[$i];
+					for ($y=0; $y < sizeof($facil_array); $y++) {
 						if(1 == count($facil_array)){
 							$suffix = "";
 						}
-						$GLOBALS['facilitator_names'] .= get_post($facil_array[$i])->post_title.$suffix;
-					//extends scope for the facil loop
+						$GLOBALS['facilitator_names'] .= get_post($facil_array[$y])->post_title.$suffix;
 					}
 				}
-					//
+				
 ?>
 <article id="content" class="col-xs-12">
 	<div class="row">
@@ -54,18 +57,18 @@
 				<?php } ?>
 			</div>
 			<div class="container">
-			
-			<h2>A program suitable for</h2>
-			<ul>
-				<?php 
-				$audience = strip_tags(get_the_taxonomies()['courses_categories']);
-				$audience_array = explode(':',$audience)[1];
-				$audience_list = explode(',',$audience_array);
-				for ($i=0; $i < count($audience_list); $i++) { ?>
+				
+				<h2>A program suitable for</h2>
+				<ul>
+					<?php
+					$audience = strip_tags(get_the_taxonomies()['courses_categories']);
+					$audience_array = explode(':',$audience)[1];
+					$audience_list = explode(',',$audience_array);
+					for ($i=0; $i < count($audience_list); $i++) { ?>
 					<li><?php echo $audience_list[$i]; ?></li>
-				<?php } ?>
-			</ul>
-				</div>
+					<?php } ?>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<div class="row">
@@ -87,17 +90,17 @@
 				<?php } ?>
 				<?php if(get_field('executive_summary')) { ?>
 				<h2>Cost</h2>
-					<h4>
-					<?php $cost = strip_tags(get_field('cost')); ?>
-					<?php echo'$'.$cost." +GST"; ?>
-					</h4>
-					<!-- <h5>Sounds right for you? <strong>Select a date and registor now.</strong> </h5> -->
+				<h4>
+				<?php $cost = strip_tags(get_field('cost')); ?>
+				<?php echo'$'.$cost." +GST"; ?>
+				</h4>
+				<!-- <h5>Sounds right for you? <strong>Select a date and registor now.</strong> </h5> -->
 				<?php }  ?>
 				<?php /* if(get_field('pdf_download')){ ?>
 				<h2>Download PDF</h2>
 				<a target="_blank" href="<?php echo get_field('pdf_download')['url']; ?>">
-				<i class="dashicons-download dashicons"></i>
-				<?php echo "[PDF] ".get_field('pdf_download')['title']; ?>
+					<i class="dashicons-download dashicons"></i>
+					<?php echo "[PDF] ".get_field('pdf_download')['title']; ?>
 				</a>
 				<?php } */ ?>
 				
@@ -111,10 +114,10 @@
 							</div>
 							<div class="panel-body">
 								<ul class="list-striped">
-								<li>
+									<li>
 										<h3>Facilitator: </h3>
 										<p>
-										<?php echo $GLOBALS['facilitator_names']; ?>
+											<?php echo $GLOBALS['facilitator_names']; ?>
 										</p>
 									</li>
 									<li>
@@ -125,24 +128,22 @@
 										<h3>Delivery Method</h3>
 										<?php the_field('deliveryMethod'); ?>
 									</li>
-
 									<li>
 										<h3>Cost </h3>
 										<p>
-										<?php $cost = strip_tags(get_field('cost')); ?>
-										<?php echo'$'.$cost." +GST"; ?>
+											<?php $cost = strip_tags(get_field('cost')); ?>
+											<?php echo'$'.$cost." +GST"; ?>
 										</p>
 									</li>
 									
 									<li>
-									<h3>When </h3>
+										<h3>When </h3>
 										<form action="<?php echo site_url();?>/cart/" method="post" id="course_add_to_cart">
 											<input type="hidden" name="postid" value="<?php the_ID();?>" />
 											<input type="hidden" name="post_timing" value="<?php echo time();?>" />
 											<?php
 											if( get_field('instances') ){
 												$i = 0;
-
 												while( has_sub_field('instances') )
 												{
 													$i++;
@@ -164,14 +165,13 @@
 													}
 													
 													$instances_name = get_sub_field('instances_name');
-
-													if((get_sub_field('instances_name'))){ ?>
-														<div>
-															<input type="radio" name="programid" id="programid-<?php echo $programinstanceid ?>" <?php if(i < 1) {  echo 'checked'; }  ?> value="<?php echo $programinstanceid ?>"/>
-															<label for="programid-<?php echo $programinstanceid ?>"><?php echo $instances_name ?></label>
-														</div>
-													<?php }
-												}
+											if((get_sub_field('instances_name'))){ ?>
+											<div>
+												<input type="radio" name="programid" id="programid-<?php echo $programinstanceid ?>" <?php if(i < 1) {  echo 'checked'; }  ?> value="<?php echo $programinstanceid ?>"/>
+												<label for="programid-<?php echo $programinstanceid ?>"><?php echo $instances_name ?></label>
+											</div>
+											<?php }
+											}
 											}
 											?>
 										</form>
@@ -181,13 +181,12 @@
 							</div>
 							<div class="panel-footer">
 								<a href="#" class="link-purchase" onclick="document.getElementById('course_add_to_cart').submit();"><span class="graphic arrow-right-sm"></span> Add to cart <span class="graphic icon-cart pull-right"></span></a>
-
 							</div>
 							
 						</div>
 						<?php if (get_field('pdf_download')) { ?>
-					<a target="_blank" href="<?php echo get_field('pdf_download')['url']; ?>">More Infomation >></a>
-					<?php } ?>
+						<a target="_blank" href="<?php echo get_field('pdf_download')['url']; ?>">More Infomation >></a>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
@@ -202,25 +201,24 @@
 				
 				
 				if(false != $facil_array){
-				//use array of facilactors to get template
-					for ($i=0; $i < sizeof($facil_array); $i++) {
+				
+					for ($k=0; $k < sizeof($facil_array); $k++) {
 					//extends scope for the facil loop
-						$GLOBALS['facilitator'] = $facil_array[$i];
+						$GLOBALS['facilitator'] = $facil_array[$k];
 						$GLOBALS['facilitator_names'] .= get_post($facil_array[$i])->post_title.",";
 						get_template_part('templates/content-post-type', 'course-facilitator-for-loop');
 					}
 					unset($GLOBALS['facilitator']);
 				}
-						else {	get_template_part('templates/content', 'no-posts'); }
+							else {	get_template_part('templates/content', 'no-posts'); }
 				?>
 			</div>
 		</div>
 	</div>
 	<!-- Facilatator -->
-
 	
 	<div class="row ">
-	<!-- Date Locations -->
+		<!-- Date Locations -->
 		<div class="container">
 			<div class="col-sm-8">
 				<h2>Dates &amp; Locations</h2>
@@ -237,42 +235,40 @@
 		</div>
 		<!-- Date Locations -->
 		<!-- Related Programs -->
-
-
-	
+		
 	</div>
 	<div class="row colored-background">
-			<?php if(get_field('related_programs')) { ?>
+		<?php if(get_field('related_programs')) { ?>
 		<div class="container">
 			<div class="container facilitator-item">
-			
-			<h2>Related Programs</h2>
-								<ul>
-									<?php
-									$related_array = trim(get_post_meta($course_id, 'related_programs', true));
-									
-									if(-1 != strpos(',',$related_array)){
-										$single_related = explode(',',$related_array);
-															} else 			{
-										$single_related = array($related_array);
-									}
-									for ($i=0; $i < sizeof($single_related); $i++) {
-										if(false != getProgramId($single_related[$i])){
-											$real_id = getProgramId($single_related[$i]);
-									?>
-									
-										<li><a  href="<?php echo get_permalink( $real_id); ?>"><?php echo get_the_title( $real_id ); ?></a>
-									</li>
-									<?php 	} // empty check
-									} // for loop
-									?>
-								</ul>
-							
-						</div>
-					</div>
+				
+				<h2>Related Programs</h2>
+				<ul>
+					<?php
+					$related_array = trim(get_post_meta($course_id, 'related_programs', true));
 					
-					<?php } // if no realted courses ?>
-<!-- Related Programs -->
+					if(-1 != strpos(',',$related_array)){
+						$single_related = explode(',',$related_array);
+														} else 			{
+						$single_related = array($related_array);
+					}
+					for ($i=0; $i < sizeof($single_related); $i++) {
+						if(false != getProgramId($single_related[$i])){
+							$real_id = getProgramId($single_related[$i]);
+					?>
+					
+					<li><a  href="<?php echo get_permalink( $real_id); ?>"><?php echo get_the_title( $real_id ); ?></a>
+				</li>
+				<?php 	} // empty check
+				} // for loop
+				?>
+			</ul>
+			
+		</div>
+	</div>
+	
+	<?php } // if no realted courses ?>
+	<!-- Related Programs -->
 </div>
 <?php
 					// WP_Query arguments
@@ -281,111 +277,108 @@
 				
 						$args = array (
 						'post_type'     => 'testimonial',
-						'meta_query'    => array( 
+						'meta_query'    => array(
 						
 						array( 'key' => 'course', 'value'     => $course_id,),
 												),
 						);
 						// The Query
-						$query = new WP_Query( $args ); ?>
-				<?php
-				if ( $query->have_posts() ) { ?>
-	<!-- Testimonimals -->
-	<div class="row">
-		<div class="container">
-			<div class="col-sm-8">
-				<h2>What others are saying</h2>
-				<?php  } ?>
-				<?php
-				if ( $query->have_posts() ) {
-					while ( $query->have_posts() ) {
-						$query->the_post();
-						get_template_part('templates/content-post-type','testimonial-landscape');
-					}
+$query = new WP_Query( $args ); ?>
+<?php
+if ( $query->have_posts() ) { ?>
+<!-- Testimonimals -->
+<div class="row">
+	<div class="container">
+		<div class="col-sm-8">
+			<h2>What others are saying</h2>
+			<?php  } ?>
+			<?php
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					get_template_part('templates/content-post-type','testimonial-landscape');
 				}
-				else {
-					//get_template_part('templates/content', 'no-posts');
-				}
-					// Restore original Post Data
-					// WP_Query arguments
-				?>
-				<?php
-				if ( $query->have_posts() ) { ?>
-			</div>
+			}
+			else {
+				//get_template_part('templates/content', 'no-posts');
+			}
+				// Restore original Post Data
+				// WP_Query arguments
+			?>
+			<?php
+			if ( $query->have_posts() ) { ?>
 		</div>
 	</div>
-	<?php  } 
-				wp_reset_postdata();
+</div>
+<?php  }
+			wp_reset_postdata();
+?>
+<!-- Testimonimals -->
 
-	?>
-	<!-- Testimonimals -->
-	
-	<div class="row colored-background">
-		<div class="container">
-			<div class="col-sm-8">
-				<h2>Cancellation Policy</h2>
-				<p>If a participant registers for this program and for some unforseen reason needs to cancel or alter their registration, they should notify the QELi team immediately. Failure to do so within&nbsp;seven (7)&nbsp;business days of the program commencement date will result in their organisation being liable for the full cost of registration fees, as per QELi policy. If a participant cancels their registration for this program between&nbsp;eight (8) and thirty (30)&nbsp;business days prior to the program commencement date, then a 25% administration charge will apply.</p>
-			</div>
+<div class="row colored-background">
+	<div class="container">
+		<div class="col-sm-8">
+			<h2>Cancellation Policy</h2>
+			<p>If a participant registers for this program and for some unforseen reason needs to cancel or alter their registration, they should notify the QELi team immediately. Failure to do so within&nbsp;seven (7)&nbsp;business days of the program commencement date will result in their organisation being liable for the full cost of registration fees, as per QELi policy. If a participant cancels their registration for this program between&nbsp;eight (8) and thirty (30)&nbsp;business days prior to the program commencement date, then a 25% administration charge will apply.</p>
 		</div>
 	</div>
+</div>
 <?php /* ?>
-	<div class="row ">
-		<div class="container">
-			<div class="col-sm-8">
-				<h2>Program Extras</h2>
-				<div class="panel-group" id="accordion-outline" role="tablist">
-					<div class="panel">
-						<div class="panel-heading" role="tab" id="heading-prereq">
-							<a class="collapsed" data-toggle="collapse" data-parent="#accordion-outline" href="#collapse-prereq" aria-expanded="true" aria-controls="collapse-prereq">
-								<h3 class="panel-title">
-								<span class="graphic arrow-panel-gray"></span>Prerequisites <span class="graphic icon-toggle pull-right"></span>
-								</h3>
-							</a>
-						</div>
-						<div id="collapse-prereq" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-prereq">
-							<div class="panel-body">
-								<?php the_field('prerequisites'); ?>
-							</div>
+<div class="row ">
+	<div class="container">
+		<div class="col-sm-8">
+			<h2>Program Extras</h2>
+			<div class="panel-group" id="accordion-outline" role="tablist">
+				<div class="panel">
+					<div class="panel-heading" role="tab" id="heading-prereq">
+						<a class="collapsed" data-toggle="collapse" data-parent="#accordion-outline" href="#collapse-prereq" aria-expanded="true" aria-controls="collapse-prereq">
+							<h3 class="panel-title">
+							<span class="graphic arrow-panel-gray"></span>Prerequisites <span class="graphic icon-toggle pull-right"></span>
+							</h3>
+						</a>
+					</div>
+					<div id="collapse-prereq" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-prereq">
+						<div class="panel-body">
+							<?php the_field('prerequisites'); ?>
 						</div>
 					</div>
-					
-					<div class="panel">
-						<div class="panel-heading" role="tab" id="heading-resources">
-							<a class="collapsed" data-toggle="collapse" data-parent="#accordion-outline" href="#collapse-resources" aria-expanded="true" aria-controls="collapse-resources">
-								<h3 class="panel-title">
-								<span class="graphic arrow-panel-gray"></span>Resources <span class="graphic icon-toggle pull-right"></span>
-								</h3>
-							</a>
-						</div>
-						<div id="collapse-resources" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-resources">
-							<div class="panel-body">
-								<?php the_field('resources'); ?>
-							</div>
-						</div>
-					</div>
-
+				</div>
 				
+				<div class="panel">
+					<div class="panel-heading" role="tab" id="heading-resources">
+						<a class="collapsed" data-toggle="collapse" data-parent="#accordion-outline" href="#collapse-resources" aria-expanded="true" aria-controls="collapse-resources">
+							<h3 class="panel-title">
+							<span class="graphic arrow-panel-gray"></span>Resources <span class="graphic icon-toggle pull-right"></span>
+							</h3>
+						</a>
+					</div>
+					<div id="collapse-resources" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-resources">
+						<div class="panel-body">
+							<?php the_field('resources'); ?>
+						</div>
+					</div>
 				</div>
+				
 			</div>
 		</div>
 	</div>
-	*/ ?>
+</div>
+*/ ?>
 <?php /* ?>
-	
-	<div class="row colored-background">
-		<div class="container">
-			<div class="col-sm-8">
-				<h2>FAQs</h2>
-				<?php if(get_field('faqs')){ ?>
-				<div class="panel-group" id="accordion-faq" role="tablist">
-					<?php
-					the_field('faqs');
-					?>
-				</div>
-	<?php } ?>
 
+<div class="row colored-background">
+	<div class="container">
+		<div class="col-sm-8">
+			<h2>FAQs</h2>
+			<?php if(get_field('faqs')){ ?>
+			<div class="panel-group" id="accordion-faq" role="tablist">
+				<?php
+				the_field('faqs');
+				?>
 			</div>
+			<?php } ?>
 		</div>
 	</div>
+</div>
 <?php	*/ ?>
 </article>
