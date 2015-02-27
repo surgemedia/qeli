3b0e5157231102db93a68d73af95dbc95bd54d22
Alex <alex.king@surgemedia.com.au>
2015-02-27 11:14:23 +1000

    BH324 - remove 'and' plus the full stop
    Please remove 'and' plus the full stop from all last bullet point on
    each program.
    I.e. and Teacher Leader.

--- templates/content-post-type-courses.php
+++ templates/content-post-type-courses.php
@@ -1,25 +1,24 @@
 <?php
-  $audience = strip_tags(get_the_taxonomies()['courses_categories']);
-  $tags = strip_tags(get_the_taxonomies()['courses_tags']);
-  $tags_array = explode(':',$tags)[1];
-
-  $audience_array = explode(':',$audience)[1];
-  $audience_list = explode(',',$audience_array);
-
-  ?>
-<div class="course col-xs-12 col-sm-6" 
-    data-audience="<?php echo $audience_array; ?>"
-    data-fee="<?php echo strip_tags(get_field('cost')); ?>"
-    data-tags="<?php echo $tags_array; ?>"
-    <?php /* Removed from Course - Alex  ?>
-    data-delivery="<?php echo strip_tags(get_field('deliveryMethod')); ?>" 
-     data-location="<?php echo strip_tags(get_field('instances')[0]['location']); ?>"
-    data-month="<?php echo get_field('instances')[0]['date']; ?>" 
-    data-development="<?php the_field('development'); ?>" 
-    <?php */ ?>
-    >
-    <div class="row">
-    <div class="col-xs-12">   
+$audience = strip_tags(get_the_taxonomies()['courses_categories']);
+$tags = strip_tags(get_the_taxonomies()['courses_tags']);
+$tags_array = explode(':',$tags)[1];
+$audience_array = explode(':',$audience)[1];
+$audience_list = explode(',',$audience_array);
+$audience_list[sizeof($audience_list)-1] = substr($audience_list[sizeof($audience_list)-1],0,-1);
+?>
+<div class="course col-xs-12 col-sm-6"
+  data-audience="<?php echo $audience_array; ?>"
+  data-fee="<?php echo strip_tags(get_field('cost')); ?>"
+  data-tags="<?php echo $tags_array; ?>"
+  <?php /* Removed from Course - Alex  ?>
+  data-delivery="<?php echo strip_tags(get_field('deliveryMethod')); ?>"
+  data-location="<?php echo strip_tags(get_field('instances')[0]['location']); ?>"
+  data-month="<?php echo get_field('instances')[0]['date']; ?>"
+  data-development="<?php the_field('development'); ?>"
+  <?php */ ?>
+  >
+  <div class="row">
+    <div class="col-xs-12">
       <h2><a href="<?php the_permalink() ?> "><?php the_title(); ?></a></h2>
       <!--
       <a href="<?php //echo site_url(); ?>/cart/" class="graphic icon-cart pull-right"></a>
@@ -27,9 +26,8 @@
       -->
       <span class="graphic icon-star pull-right"></span>
     </div>
-
     <hr class="col-xs-12">
-     <?php /*  Removed from Course - Alex ?>
+    <?php /*  Removed from Course - Alex ?>
     <div class="col-xs-12 col-sm-6">
       <?php truncate(get_field('executive_summary'),50,"...");?>
       <a href="<?php echo the_permalink(); ?>" class="btn btn-program"><span class="graphic icon-read-more"></span> Read More</a>
@@ -47,14 +45,21 @@
           <tr>
             <td><b>Audience</b></td>
             <td>
-            <ul>
-            <?php 
-        
-        for ($i=0; $i < count($audience_list); $i++) { ?>
-          <li><?php echo $audience_list[$i]; ?></li>
-        <?php } ?>
-        </ul>
-        </td>
+              <ul>
+                <?php
+                
+                for ($i=0; $i < count($audience_list); $i++) { ?>
+                <?php
+               
+                if(1 == strpos($audience_list[$i],'and ')){ ?>
+                <li><?php echo substr($audience_list[$i], 5); ?></li>
+                <?php } else { ?>
+                <li><?php echo $audience_list[$i]; ?></li>
+                <?php } ?>
+                <?php } ?>
+
+              </ul>
+            </td>
           </tr>
           <tr>
             <td><b>Fees</b></td>
@@ -66,7 +71,7 @@
             <td><?php echo get_field('locations'); ?></td>
           </tr>
           
-          <tr> 
+          <tr>
             <td><b>Month</b></td>
             <td><?php echo get_field('instances')[0]['instances_name']; ?></td>
           </tr>
@@ -79,6 +84,8 @@
         </tbody>
       </table>
     </div>
-  <hr class="col-xs-12">
+    <hr class="col-xs-12">
   </div>
+  <?php
+  ?>
 </div>
