e1e6564c9fd4b405906bdcb30622ac8afe2a5972
Jake Waterer <jake.w@breadcrumbdigital.com.au>
2015-02-27 15:46:54 +1000

    minor alts for video

--- templates/content-post-type-video.php
+++ templates/content-post-type-video.php
@@ -2,7 +2,7 @@
 <div class=" col-xs-12 col-sm-6 col-md-6">
   <div class="video-item col-sm-4">
   <a href="#" class="video-popup-thumbnail" data-url="//www.youtube.com/embed/<?php echo cleanYoutubeLink(get_field('talks')[0]['embed_code']); ?>" data-modal="#videoModal" data-width="400" data-height="225" role="button">
-    <img alt="400x225" class="img-responsive" src="<?php echo getFeaturedUrl(null, 'qeli-talks-square'); ?>" data-holder-rendered="true">
+    <img alt="400x225" height="200" width="200" class="img-responsive" src="<?php echo getFeaturedUrl(null, 'qeli-talks-square'); ?>" data-holder-rendered="true">
     <div class="overlay">
       <span class="graphic btn-play-sm"></span>
     </div>
