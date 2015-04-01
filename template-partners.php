dad5404c3028f6be853be478039138d6b2482081
Alex <alex.king@surgemedia.com.au>
2015-04-01 12:31:18 +1000

    #BH364 - Alphabetical Partner page.
    19Mins sort alphabetical

--- template-partners.php
+++ template-partners.php
@@ -14,27 +14,37 @@ Template Name: Partners
 		</div>
 	</div>
 
-	<?php if(have_rows('partners')): $i = 0; ?>
-		<?php while(have_rows('partners')): the_row();?>
+	<?php 
+	//Get the repeater
+		$repeater = get_field('partners');
+	//Order is empty for sorting
+		$order = array();
+
+		foreach( $repeater as $x => $row ) {
+	//Order setup the order
+		$order[ $x ] = $row['name'];
+		} 
+	//Sorts into Alphabetical 
+		array_multisort( $order, SORT_ASC, $repeater );
+		for ($i=0; $i < count($repeater); $i++):  
+	?>
 			<div class="container">
 				<div class="row">
-					<?php $i++; ?>
 					<div class="col-xs-6 col-sm-3">
-						<img src="<?php $id = get_sub_field('image');
+						<img src="<?php $id = $repeater[$i]['image'];
 								// TODO this is not returning the correct sized image
 									echo wp_get_attachment_image_src($id, 'thumnbnail')[0];
 						?>" class="img-responsive"/>
 					</div>
 					<div class="col-xs-12 col-sm-9">
 						<!-- <div class="numbering"><?php // echo $i; ?></div> -->
-						<h2><?php the_sub_field('name'); ?></h2>
-						<?php the_sub_field('description'); ?>
+						<h2><?php echo $repeater[$i]['name']; ?></h2>
+						<?php echo $repeater[$i]['description']; ?>
 						<a href="<?php the_sub_field('homepage'); ?>" target="_blank"><span class="graphic arrow-right-outline"></span><?php the_sub_field('homepage'); ?></a>
 					</div>
 				</div>
 			</div>
 			<hr>
-		<?php endwhile; ?>
-	<?php endif; ?>
+	<?php endfor; ?>
 </article>
 <?php endwhile; ?>
