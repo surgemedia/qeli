33bad4fcdbdb969177e47177aed7535df43c18e7
Alex <surge@aegirs-iMac.local>
2015-11-13 16:48:23 +1000

    Added New Taxomney - archive_tax removed default sidebar for taxonomy lister
    https://app.asana.com/0/37701276853631/63228654965534

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -1,36 +1,55 @@
-
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-9">
-				<?php
-
-				// TODO JW - this query should pull the news items published in the last 7 days
-				// WP_Query arguments
-				$args = array (
-				'post_type'              => 'newsletter',
-				'pagination'             => false,
-				'posts_per_page'         => '12',
-				);
-				// The Query
-				$query = new WP_Query( $args );
-				if ( $query->have_posts() ) {
-						while ( $query->have_posts() ) {
-						$query->the_post();
-						get_template_part('templates/content-post-type-post-block-news', 'listing');
-						}
-						} else {
-						get_template_part('templates/content', 'no-posts');
-						}
-						// Restore original Post Data
-					wp_reset_postdata();
-				?>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-			    <?php if (roots_display_sidebar()) : ?>
-			        <aside class="sidebar" role="complementary">
-			          <?php include roots_sidebar_path(); ?>
-			        </aside><!-- /.sidebar -->
-	  			<?php endif; ?>
-			</div>
-		</div>
-	</div>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-9">
+            <?php
+            // TODO JW - this query should pull the news items published in the last 7 days
+            // WP_Query arguments
+            $args = array (
+            'post_type'              => 'newsletter',
+            'pagination'             => false,
+            'posts_per_page'         => '12',
+            );
+            // The Query
+            $query = new WP_Query( $args );
+            if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+            $query->the_post();
+            get_template_part('templates/content-post-type-post-block-news', 'listing');
+            }
+            } else {
+            get_template_part('templates/content', 'no-posts');
+            }
+            // Restore original Post Data
+            wp_reset_postdata();
+            ?>
+        </div>
+        <div class="col-xs-12 col-sm-3">
+            <?php if (roots_display_sidebar()) : ?>
+            <aside class="sidebar" role="complementary">
+                <?php // include roots_sidebar_path(); ?>
+               
+                    <section class="widget search-2 widget_search">
+                    <?php 
+            		get_template_part('templates/searchform');
+                     ?>
+                </section>
+	                <section class="widget archives widget_archive">
+	                <h3>Archives</h3>
+	             <?php 
+	              $terms = get_terms( 'archive_tax' );
+	              
+					 if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+					     echo '<ul>';
+					     foreach ( $terms as $term ) {
+					       echo '<li>' . $term->name . '</li>';
+					     }
+					     echo '</ul>';
+					 }
+					  ?>
+	        		</section>
+      
+        </aside><!-- /.sidebar -->
+        <?php endif; ?>
+    </div>
+</div>
+</div>
--- lib/action-post-type-newsletter.php
+++ lib/action-post-type-newsletter.php
@@ -1,4 +1,43 @@
 <?php
+// Register Custom Taxonomy
+function archive_tax() {
+
+	$labels = array(
+		'name'                       => _x( 'Archives', 'Taxonomy General Name', 'archive_tax' ),
+		'singular_name'              => _x( 'Archive', 'Taxonomy Singular Name', 'archive_tax' ),
+		'menu_name'                  => __( 'Archive', 'archive_tax' ),
+		'all_items'                  => __( 'All Items', 'archive_tax' ),
+		'parent_item'                => __( 'Parent Item', 'archive_tax' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'archive_tax' ),
+		'new_item_name'              => __( 'New Item Archive', 'archive_tax' ),
+		'add_new_item'               => __( 'Add New Archive', 'archive_tax' ),
+		'edit_item'                  => __( 'Edit Archive', 'archive_tax' ),
+		'update_item'                => __( 'Update Archive', 'archive_tax' ),
+		'view_item'                  => __( 'View Archive', 'archive_tax' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'archive_tax' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'archive_tax' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'archive_tax' ),
+		'popular_items'              => __( 'Popular Items', 'archive_tax' ),
+		'search_items'               => __( 'Search Items', 'archive_tax' ),
+		'not_found'                  => __( 'Not Found', 'archive_tax' ),
+		'items_list'                 => __( 'Items list', 'archive_tax' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'archive_tax' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'archive_tax', array( 'newsletter' ), $args );
+
+}
+add_action( 'init', 'archive_tax', 0 );
+
+
 // Register Custom Post Type
 function newsletter_posts() {
 
@@ -44,3 +83,5 @@ function newsletter_posts() {
 
 // Hook into the 'init' action
 add_action( 'init', 'newsletter_posts', 0 );
+
+
