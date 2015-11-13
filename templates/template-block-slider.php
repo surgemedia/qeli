aef57f74971283e86d20df1c0fe287d1f2460ddb
Alex <surge@aegirs-iMac.local>
2015-11-13 16:49:00 +1000

    Home page Promotion panel - make link update option - 0.5 (support)
    https://app.asana.com/0/37701276853631/63228654965534

--- templates/template-block-slider.php
+++ templates/template-block-slider.php
@@ -1,7 +1,12 @@
-<div class="section hero">
-<!--<img data-src="holder.js/1440x475/auto/gray" alt="..." class="img-responsive">-->
-  <?php //debug(get_field('sliders')) ?>
+<?php 
+/*====================================
+=            Section Hero            =
+====================================*/
 
+
+
+ ?>
+<div class="section hero">
   <div class="home-images" style="background-image: url('<?php echo get_field('sliders')['0']['image']['url']; ?>')">
   <div class="container">
     <h1> <?php echo get_field('sliders')['0']['header_text']; ?></h1>
@@ -9,7 +14,9 @@
     <div class="call-to-action">
       <div class="container">
           <div class="col-xs-8"><?php echo get_field('sliders')['0']['footer_text']; ?></div>
-          <div class="col-xs-4 text-right"><a href="<?php echo get_permalink( 157 ); ?>" class="big-link"><span class="graphic arrow-link-sq-white"></span> See our solutions</a></div>
+          <div class="col-xs-4 text-right"><a href="<?php echo get_field('sliders')['0']['arrow_link']; ?>" class="big-link">
+            <span class="graphic arrow-link-sq-white"></span><?php echo get_field('sliders')['0']['arrow_text']; ?></a>
+          </div>A
       </div>
     </div>
   </div>
