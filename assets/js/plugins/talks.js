0406116ff8cc7d3149ea098b971cc4b1c8c11f47
Alex <surge@aegirs-iMac.local>
2015-11-16 08:06:11 +1000

    Added Filtering for QELi Talks using isometric

--- assets/js/plugins/talks.js
+++ assets/js/plugins/talks.js
@@ -11,24 +11,25 @@ $(document).ready(function () {
 		filterList = document.querySelectorAll('.video-filter a');
 		videoNavList = document.querySelectorAll('.video-nav a');
 
-
 		for(var i = 0; i < filterList.length; i++) {
 			filterList[i].onclick = function(event) {
 				(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
 
-				
-
 				$(filterList).removeClass('selected');
 				$(this).addClass('selected'); //working
+				$('.clearfix').addClass('out');
 
 				var term = this.getAttribute('data-term');
 				console.log(term);
 				if (term !== 'clear') {
 					$(videoList).addClass('out');
+
 					for(var i = 0; i < videoList.length; i++) {
 						//if (term === videoList[i].getAttribute('data-term')) {
 						if ((videoList[i].getAttribute('data-term')).indexOf(term) != -1) {
 							$(videoList[i]).removeClass('out');
+							
+
 						}
 					}
 					$(videoNavList).addClass('out');
@@ -36,10 +37,13 @@ $(document).ready(function () {
 						//if (term === videoNavList[i].getAttribute('data-term')) {
 						if ((videoNavList[i].getAttribute('data-term')).indexOf(term) != -1) {
 							$(videoNavList[i]).removeClass('out');
+							
+
 						}
 					}
 				}
 				else {
+					$('.clearfix').removeClass('out');
 					$(videoList).removeClass('out');
 					$(videoNavList).removeClass('out');
 				}
